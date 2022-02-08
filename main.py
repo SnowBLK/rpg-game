83b7c3ed29afcfe9d37d75c4046c9deb68f130d6
Nesrinezid <98804757+Nesrinezid@users.noreply.github.com>
2022-02-08 11:23:58 +0100

    constructeur qui marche

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+
+class Wizard:
+
+        def __init__(self,degat, chance,fuite,prix,unit_type):
+        degat=random.randint(2,4)
+        chance=20
+        fuite=10
+        prix=15
+        unit_type="wizard"
