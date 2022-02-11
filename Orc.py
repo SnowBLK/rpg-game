6057f8540e1b2fd1c031c639d142ede059f540ae
‘Macyl_BENZERRA’ <snowblack.steam@gmail.com>
2022-02-11 13:41:17 +0100

    Added main method and tested Orc class

--- Orc.py
+++ Orc.py
@@ -23,3 +23,16 @@ class Orc:
     @loot.setter
     def loot(self, value):
         self.__loot = value
+
+
+if __name__ == "__main__":
+    o = Orc()
+    l = Orc()
+
+    print("O :")
+    print(o.degat)  # 3 or 4 or 5
+    print(o.loot)  # 1 or 2
+
+    print("L :")
+    print(l.degat)  # 3 or 4 or 5
+    print(l.loot)  # 1 or 2
